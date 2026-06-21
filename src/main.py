c6d38f03353d24e4f6918f99e83fa0e22c0e4e0f
Hayala Hora <horahayala@gmail.com>
2026-06-21 16:30:06 -0300

    feat: add interactive menu and main function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,40 @@
+from stock import cadastrar_item
+
+
 def mostrar_menu():
-    pass
+    while True:
+        print("\n===== UNISTOCK =====")
+        print("[1] - Cadastrar item")
+        print("[2] - Entrada")
+        print("[3] - Saída")
+        print("[4] - Sair")
+
+        opcao = input("\nSelecione uma ação: ").strip()
+
+        if opcao in ["1", "2", "3", "4"]:
+            return opcao
+
+        print("\nOpção inválida. Tente novamente.")
 
 
-while True:
-    mostrar_menu()
+def main():
+    estoque = {}
+
+    while True:
+        opcao = mostrar_menu()
+
+        if opcao == "1":
+            cadastrar_item(estoque)
+
+        elif opcao == "2":
+            print("\nEntrada ainda não implementada.")
+
+        elif opcao == "3":
+            print("\nSaída ainda não implementada.")
+
+        elif opcao == "4":
+            print("\nEncerrando sistema...")
+            break
+
+
+main()
--- src/stock.py
+++ src/stock.py
@@ -1,4 +1,39 @@
-cadastrar_item()
-buscar_item()
-entrada_item()
-saida_item()
+def cadastrar_item(estoque):
+
+    while True:
+
+        codigo = input("\nCódigo do item: ").strip()
+
+        if codigo == "":
+            print("\nO código não pode estar vazio.")
+            continue
+        
+        if codigo.isdigit() == 0:
+            print("\nO código não pode ser zero.")
+            continue
+
+
+        if codigo in estoque:
+            print("\nCódigo já cadastrado.")
+            continue
+
+        break
+
+    while True:
+
+        nome = input("Nome do item: ").strip()
+
+        if nome == "":
+            print("\nO nome não pode estar vazio.")
+            continue
+
+        break
+
+    estoque[codigo] = {
+        "nome": nome,
+        "quantidade": 0
+    }
+
+    print("\nItem cadastrado com sucesso.")
+
+    input("\nPressione ENTER para voltar ao menu...")
